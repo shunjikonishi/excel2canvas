8fbb96ce7a23764290793d28679708fff13730c1
k-shunji <k-shunji@flect.co.jp>
2014-05-16 15:20:58 +0900

    Bugfix

--- src/main/java/jp/co/flect/excel2canvas/NamedValueExtractor.java
+++ src/main/java/jp/co/flect/excel2canvas/NamedValueExtractor.java
@@ -8,11 +8,14 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.util.CellReference;
+import com.google.gson.Gson;
 
 public class NamedValueExtractor {
 
@@ -42,11 +45,22 @@ public class NamedValueExtractor {
 		List<NamedCellInfo> list = ExcelUtils.createNamedCellList(workbook);
 		Map<String, Object[]> map = new HashMap<String, Object[]>();
 		for (NamedCellInfo info : list) {
-			Sheet sheet = workbook.getSheet(info.getSheetName());
+			int currentLine = -1;
+			int includeSize = 0;
+			boolean hasValue = false;
+			Sheet sheet = workbook.getSheet(info.
+				getSheetName());
 			List<Object> values = new ArrayList<Object>();
 			for (CellReference cRef : info.getCellList()) {
 				Cell cell = ExcelUtils.getOrCreateCell(sheet, cRef.getRow(), cRef.getCol());
-				if (cell.getCellType() == Cell.CELL_TYPE_FORMULA && !includeFormulaValue) {
+				if (cRef.getRow() != currentLine) {
+					currentLine = cRef.getRow();
+					if (hasValue) {
+						includeSize = values.size();
+					}
+					hasValue = false;
+				}
+				if (cell.getCellType() == Cell.CELL_TYPE_FORMULA && info.getCellList().size() > 1 && !includeFormulaValue) {
 					continue;
 				}
 				FormattedValue fv = helper.getFormattedValue(cell);
@@ -71,9 +85,15 @@ public class NamedValueExtractor {
 							break;
 					}
 				}
+				if (value != null && value.toString().length() > 0) {
+					hasValue = true;
+				}
 				values.add(value);
 			}
-			map.put(info.getName(), values.toArray());
+			if (hasValue) {
+				includeSize = values.size();
+			}
+			map.put(info.getName(), values.subList(0, includeSize).toArray());
 		}
 		return map;
 	}
@@ -84,5 +104,35 @@ public class NamedValueExtractor {
 
 	public Map<String, Object[]> extract(InputStream is) throws IOException, InvalidFormatException {
 		return extract(ExcelUtils.createWorkbook(is));
+	}
+
+	public static String toJson(Map<String, Object[]> map) {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sss");
+		Map<String, Object> ret = new HashMap<String, Object>();
+		for (Map.Entry<String, Object[]> entry : map.entrySet()) {
+			String key = entry.getKey();
+			Object[] values = entry.getValue();
+			if (values.length == 0) {
+				ret.put(key, null);
+			} else if (values.length == 1) {
+				ret.put(key, convert(values[0], format));
+			} else {
+				Object[] newValues = new Object[values.length];
+				for (int i=0; i<values.length; i++) {
+					newValues[i] = convert(values[i], format);
+				}
+				ret.put(key, newValues);
+			}
+		}
+		return new Gson().toJson(ret);
+	}
+
+	private static Object convert(Object o, SimpleDateFormat format) {
+		if (o instanceof Date) {
+			return format.format((Date)o);
+		} else if (o instanceof String && o.toString().length() == 0) {
+			return null;
+		}
+		return o;
 	}
 }
